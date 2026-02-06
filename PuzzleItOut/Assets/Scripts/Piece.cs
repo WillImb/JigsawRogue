4030791819e8120e9c1274570834aeb34ddcce93
Rishit Roshan <92199045+RisTEC@users.noreply.github.com>
2026-02-06 09:26:28 -0500

    chore: moved gitignore and added code for pieces and piecescriptable

---
+++ PuzzleItOut/Assets/Scripts/Piece.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public PieceScriptable pieceData;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
