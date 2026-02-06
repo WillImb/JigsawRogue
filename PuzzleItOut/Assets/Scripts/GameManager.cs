4c1c07ae8da511992f991872a0b63d6bb009c8ef
WillImb <143808397+WillImb@users.noreply.github.com>
2026-02-06 10:04:46 -0500

    Game Manager created

---
+++ PuzzleItOut/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public float playerHealth;
+    public float money;
+
+    public bool paused = false;
+    public int turn; //0 for player 1 for enemy;
+
+    public GameObject currentEnemy; // change to type enemy when rish adds script
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            paused = !paused;
+        }
+    }
+}
