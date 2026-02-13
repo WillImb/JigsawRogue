88f83a7a2bf88e79d5926c1f9549a571f8dda2a0
Phillip Brown <pkb5886@rit.edu>
2026-02-13 09:48:37 -0500

    Update CombatManager.cs

--- PuzzleItOut/Assets/Scripts/CombatManager.cs
+++ PuzzleItOut/Assets/Scripts/CombatManager.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
     public static CombatManager Instance;
+
+    //List<ComboScriptable> ComboList;
 
     private void Awake()
     {
@@ -33,5 +36,15 @@ public class CombatManager : MonoBehaviour
     public void TakeDmg(float damage)
     {
         return;
+    }
+    private float CalculateDmg()
+    {
+        float damage = 0;
+        return damage;
+    }
+
+    public void FindCombo(Piece[] pieces)
+    {
+
     }
 }
