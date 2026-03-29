3529a5b31b19896a75a71590ab5197dd06484ec1
anthonyL04 <aml6963@g.rit.edu>
2026-03-29 13:44:51 -0400

    Updating branch

--- PuzzleItOut/Assets/Scripts/ShopManager.cs
+++ PuzzleItOut/Assets/Scripts/ShopManager.cs
@@ -1,6 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class ShopManager
+public class ShopManager : MonoBehaviour
 {
-    
+    public List<GameObject> pieces;
+    public List<GameObject> upgrades;
+    public List<GameObject> combos;
+
+    public GameObject spellBook;
+
+    void Start()
+    {
+        RandomizeListUnique(pieces);
+        RandomizeListUnique(upgrades);
+        RandomizeListUnique(combos);
+
+        spellBook = GameObject.FindWithTag("SpellBook");
+    }
+
+    void RandomizeListUnique(List<GameObject> list)
+    {
+        // create a pool of numbers 1–5
+        List<int> numberPool = new List<int> { 1, 2, 3, 4, 5 };
+
+        // shuffle the pool
+        for (int i = 0; i < numberPool.Count; i++)
+        {
+            int randomIndex = Random.Range(i, numberPool.Count);
+            int temp = numberPool[i];
+            numberPool[i] = numberPool[randomIndex];
+            numberPool[randomIndex] = temp;
+        }
+
+        // assign first N unique values (N = list count, e.g., 3)
+        for (int i = 0; i < list.Count; i++)
+        {
+            TMP_Text text = list[i].GetComponentInChildren<TMP_Text>();
+            if (text != null)
+            {
+                text.text = numberPool[i].ToString();
+            }
+        }
+    }
 }
--- PuzzleItOut/Assets/Scripts/SpellBook.cs
+++ PuzzleItOut/Assets/Scripts/SpellBook.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Spellbook : MonoBehaviour
 {
@@ -11,19 +12,28 @@ public class Spellbook : MonoBehaviour
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
-
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 
+    // nothing done in on scene loaded atm
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // nothing done in on scene loaded atm
     }
 
     public bool ComboUnlocked(ComboScriptable combo)
