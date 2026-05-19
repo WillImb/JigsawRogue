7fceb0d24b69d4b5c02a2d8275e4f63ebba37733
EnderTheOne <aml6963@g.rit.edu>
2026-05-19 11:15:03 -0400

    Finished early mana implementation
    - Combos do not require mana to work atm - but the bones for mana have been implemented.
    - Text for mana and health have been added for easier reference
    - SliderManager has been added to also manage health slider for enemy
    - Enemy has been updated so it no longer handles and GUI related tasks (it still manages VFX, though)
    - GameScene has been updated to include text objects for health and mana. Also has been slightly cleaned up and reorganized

--- PuzzleItOut/Assets/Scripts/SliderManager.cs
+++ PuzzleItOut/Assets/Scripts/SliderManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 /*
  * Class: SliderManager
@@ -11,27 +12,44 @@ using UnityEngine.UI;
 public class SliderManager : MonoBehaviour
 {
     Player player;
+    Enemy enemy;
 
     public Slider playerHealthSlider;
     public Slider playerManaSlider;
 
+    public Slider enemyHealthSlider;
+
+    [SerializeField] TextMeshProUGUI playerHealthText;
+    [SerializeField] TextMeshProUGUI playerManaText;
+    [SerializeField] TextMeshProUGUI enemyHealthText;
+
     void Start()
     {
+        // assign local refernces
         player = Player.instance;
+        enemy = Enemy.instance;
 
+        // subscribe to events
         player.OnHealthChanged += UpdatePlayerHealthSlider;
         player.OnManaChanged += UpdatePlayerManaSlider;
+        enemy.OnHealthChanged += UpdateEnemyHealthSlider;
 
+        // update GUI for game start
         UpdatePlayerHealthSlider(player.GetHealth(), player.maxHealth);
         UpdatePlayerManaSlider(player.GetMana(), player.maxMana);
+        UpdateEnemyHealthSlider(enemy.health, enemy.maxHealth);
     }
 
     void OnDestroy()
     {
         player.OnHealthChanged -= UpdatePlayerHealthSlider;
         player.OnManaChanged -= UpdatePlayerManaSlider;
+        enemy.OnHealthChanged -= UpdateEnemyHealthSlider;
     }
 
+    /// <summary>
+    /// Generic slider update method
+    /// </summary>
     void UpdateSlider(Slider slider, int value, int maxValue)
     {
         slider.value = value / (float)maxValue;
@@ -39,11 +57,23 @@ public class SliderManager : MonoBehaviour
 
     void UpdatePlayerHealthSlider(int value, int maxValue)
     {
+        // refresh slider ui
         UpdateSlider(playerHealthSlider, value, maxValue);
+        // update text
+        playerHealthText.text = player.health + "/" + player.maxHealth;
     }
 
     void UpdatePlayerManaSlider(int value, int maxValue)
     {
         UpdateSlider(playerManaSlider, value, maxValue);
+        playerManaText.text = player.mana + "/" + player.maxMana;
+    }
+
+    void UpdateEnemyHealthSlider(int value, int maxValue)
+    {
+        // refresh slider ui
+        UpdateSlider(enemyHealthSlider, value, maxValue);
+        // update text
+        enemyHealthText.text = enemy.health + "/" + enemy.maxHealth;
     }
 }
