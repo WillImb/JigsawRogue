1b999548e3288fe02c8399506e9451548de20665
EnderTheOne <aml6963@g.rit.edu>
2026-05-27 10:48:24 -0400

    Combos now require mana

--- PuzzleItOut/Assets/Scripts/ComboScriptable.cs
+++ PuzzleItOut/Assets/Scripts/ComboScriptable.cs
@@ -22,30 +22,6 @@ public class ComboScriptable : ScriptableObject
     [Header("Combo Effects")]
     public SpecialComboManager.priority priority;
     public int turns = 1;
-    
-    /*
-    public int Damage(List<PieceScriptable> pieces)
-    {
-        int sum = pieces.Sum(p => p.combatValue);
-        return activeIngredients.Contains(IngredientType.Combat) ? (int)(sum * multiplier) : sum;
-    }
 
-    public int Health(List<PieceScriptable> pieces)
-    {
-        int sum = pieces.Sum(p => p.healingValue);
-        return activeIngredients.Contains(IngredientType.Health) ? (int)(sum * multiplier) : sum;
-    }
-
-    public int Gold(List<PieceScriptable> pieces)
-    {
-        int sum = pieces.Sum(p => p.goldValue);
-        return activeIngredients.Contains(IngredientType.Gold) ? (int)(sum * multiplier) : sum;
-    }
-
-    public float Probability(List<PieceScriptable> pieces)
-    {
-        float sum = pieces.Sum(p => p.probabilityValue);
-        return activeIngredients.Contains(IngredientType.Probability) ? sum * multiplier : sum;
-    }
-    */
+    public int manaCost;
 }
